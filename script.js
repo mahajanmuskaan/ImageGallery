2a6e047f77e8aaa45fae539eb8a537780de97ad2
mahajanmuskaan <er.muskaan21@gmail.com>
2023-05-12 13:21:20 +0530

    Committing objects

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav_list");
+const navItems = document.querySelectorAll(".nav_list_items");
+
+// Here the function mobileMenu() also adds an active class on our hamburger and our nav_list which makes our mobile menu open.
 
 hamburger.addEventListener("click", mobileMenu);
-
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navList.classList.toggle("active");
 }
+
+// The closeMenu() function removes the active class from both the nav_list and the hamburger which makes our mobile menu close.
+
+navItems.forEach(n => n.addEventListener("click", closeMenu));
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+}
+
